66eac243919d86c9cf58564ac822b128b2e98c36
MatyHuang <greenstimorol@gmail.com>
2018-06-13 02:32:50 +0800

    Add filtering

--- read.py
+++ read.py
@@ -11,3 +11,10 @@ print('檔案讀取完了！總共有', len(data), '筆資料')
 for text in data:
 	text_count += len(text)
 print('留言平均長度是', int(text_count / len(data)), '字')
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '則小於100字的留言')
+print(new[10])
