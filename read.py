550a76df9d2a43ef83878caba9b838928f697204
MatyHuang <greenstimorol@gmail.com>
2018-06-13 02:20:45 +0800

    calc avg text num

--- read.py
+++ read.py
@@ -1,5 +1,6 @@
 data = []
 count = 0
+text_count = 0
 with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
@@ -7,3 +8,6 @@ with open('reviews.txt', 'r') as f:
 		if count % 1000 == 0:
 			print(len(data))
 print('檔案讀取完了！總共有', len(data), '筆資料')
+for text in data:
+	text_count += len(text)
+print('留言平均長度是', int(text_count / len(data)), '字')
